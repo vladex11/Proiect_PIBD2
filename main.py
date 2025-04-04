4f34bc74dacb4e6b4eefc18fc4ba576d00d9f598
Vlad Marian Panait <133703150+vladex11@users.noreply.github.com>
2025-04-04 13:10:42 +0300

    main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="vladada123",  
+        password="*******",  
         database="MedicalDatabase"
     )
 
